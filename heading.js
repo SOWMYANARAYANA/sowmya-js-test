9f8479b0fa143e3ab8afac5d993f79301c401cf3
kuldeep_surendra <ksurendra@qwinix.io>
2018-03-14 17:42:13 +0530

    add files

---
+++ heading.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var x = document.getElementById("myDiv");
+    if (x.style.backgroundColor === "red") {
+        x.style.backgroundColor = "white";
+    } else {
+        x.style.backgroundColor = "red";
+    }
+}
+function sowmya() {
+    var x = document.getElementById("demo");
+    if (x.style.backgroundColor === "blue") {
+        x.style.backgroundColor = "white";
+    } else {
+        x.style.backgroundColor = "blue";
+    }
+}
+function repo() {
+    var x = document.getElementById("mid");
+    if (x.style.backgroundColor === "green") {
+        x.style.backgroundColor = "white";
+    } else {
+        x.style.backgroundColor = "green";
+    }
+}
